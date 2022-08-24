3e4df49d0e1dbed176c4a6dd95a21996a623e460
Mohd Osama Iqbal <osamaiqbal786@gmail.com>
2022-08-24 11:42:01 +0530

    middleware updated

--- middleware/index.js
+++ middleware/index.js
@@ -1,25 +1,23 @@
-var middlewareobj={}
+var middlewareobj = {};
 
-middlewareobj.isloggedin=function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
+middlewareobj.isloggedin = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect("/login");
+};
+
+middlewareobj.isauthorised = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    if (req.user.isadmin.toString() === "true") {
+      return next();
+    } else {
+      res.redirect("/foods");
     }
-    // req.flash("error","please log in first");
+  } else {
     res.redirect("/login");
-}
+  }
+};
 
-middlewareobj.isauthorised=function (req, res, next){
-    if(req.isAuthenticated()){
-        if(req.user.isadmin.toString()==="true"){
-        return next();
-        }else{
-            res.redirect("/foods");
-        }
-    
-    }else{
-        res.redirect("/login");
-    }
-        
-}
-
-module.exports= middlewareobj
+module.exports = middlewareobj;
